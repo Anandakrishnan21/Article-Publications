984d8b434d8a44cd4be4d3054d75d15a4791a753
Anandakrishnan Dinej <anandakrishnandinej02@gmail.com>
2023-11-16 00:48:13 +0530

    users schema created

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    affiliation: {
+      type: String,
+      required: true,
+    },
+    scholar: {
+      type: String,
+    },
+    scopus:{
+      type: Number,
+    },
+    orcid: {
+      type: Number,
+    }
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
