13cfec8f9fb11265fd3161cc3d3c37833d2fb5f4
Anandakrishnan21 <121444809+Anandakrishnan21@users.noreply.github.com>
2024-05-02 09:49:45 +0530

    Update loading.jsx

--- src/app/home/loading.jsx
+++ src/app/home/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
   return (
-    <div className="h-[520px] flex justify-center items-center">
+    <div className="w-screen md:w-full h-[520px] flex justify-center items-center dark:bg-neutral-950">
       <div className="loader"></div>
     </div>
   );
